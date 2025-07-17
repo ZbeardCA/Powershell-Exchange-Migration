941e7bc987ccf1e307923b932c273fa62e2c2d96
ZbeardCA <zaur.bahsaliev@cloud-advice.com>
2025-07-17 09:43:58 +0200

    Update Exchange_Basics.ps1

--- Exchange_Basics.ps1
+++ Exchange_Basics.ps1
@@ -1,5 +1,9 @@
-# Anlegen vom Relay
+# Anlegen vom Anonymous Relay
+
+
 New-ReceiveConnector -Name "Anonymous Relay" -TransportRole FrontendTransport -Custom -Bindings 0.0.0.0:25 -RemoteIpRanges 192.168.100.50, 192.168.100.51
 Set-ReceiveConnector "Anonymous Relay" -PermissionGroups AnonymousUsers
 Get-ReceiveConnector "Anonymous Relay" | Add-ADPermission -User "NT-Authority\Anonymous-Logon" -ExtendedRights "Ms-Exch-SMTP-Accept-Any-Recipient"
 
+#oder in Englisch
+Get-ReceiveConnector "Anonymous Relay" | Add-ADPermission -User "NT AUTHORITY\ANONYMOUS LOGON" -ExtendedRights "Ms-Exch-SMTP-Accept-Any-Recipient"
