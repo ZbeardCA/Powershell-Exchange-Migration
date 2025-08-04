81966485814ade6440e42a13a3756f4e20540f64
ZbeardCA <zaur.bahsaliev@cloud-advice.com>
2025-08-04 13:07:33 +0200

    Update and rename Exchange_Basics.ps1 to ExchangePowershell_Basics.ps1

--- ExchangePowershell_Basics.ps1
+++ ExchangePowershell_Basics.ps1
@@ -32,7 +32,4 @@ Get-Command Exsetup.exe | ForEach-Object {$_.FileVersionInfo}
 #URL
 https://learn.microsoft.com/de-de/exchange/new-features/build-numbers-and-release-dates
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
-#TTL im Windows DNS anpassen
-Get-DnsServerResourceRecord -zonename "ZONENNAME"
-
-Set-DnsServerResourceRecord -ZoneName "autodiscover.ekl-ag.de" -RRType "A" -Name "(same as parent folder)" -NewTimeToLive 00:05:00
+#
