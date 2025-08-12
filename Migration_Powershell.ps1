12a4ebde1da4bf5d4e471818f4d3bfba7e001a4d
ZbeardCA <zaur.bahsaliev@cloud-advice.com>
2025-08-12 16:41:14 +0200

    Update Migration_Powershell.ps1

--- Migration_Powershell.ps1
+++ Migration_Powershell.ps1
@@ -74,5 +74,48 @@ Get-MoveRequest -BatchName "BATCHNAME" -MoveStatus Synced
 
 #Datenbanken umbennenen und mit Logs verschieben
 
-Get-MailboxDatabase -Server SERVERNAME | Set-MailboxDatabase -Name DATENBANKNAME
-Move-DatabasePath DATENBANKNAME -EdbFilePath E:\xxx\xxx.edb -LogFolderPath F:\xxx
+Get-MailboxDatabase -Server "SERVERNAME" | Set-MailboxDatabase -Name MDB01
+Move-DatabasePath MDB01 -EdbFilePath E:\MDB01\MDB01.edb -LogFolderPath F:\MDB01
+
+#Erstellen einer neuen Datenbank und Logfolderpath
+
+New-MailboxDatabase -Name "MDB01" -Server "SERVERNAME" -EdbFilePath "E:\MDB01\MDB01.edb" -LogFolderPath "F:\MDB01"
+
+#Virtuelle Verzeichnisse kopieren
+
+$Exchange2019Server = "NEUERSERVER"
+$Exchange2016Server = "ALTERSERVER"
+#Get URLs from Exchange 2016 Server
+$autodiscoverhostname = (Get-ClientAccessService $Exchange2016Server).AutoDiscoverServiceInternalUri
+$owainturl = (Get-OwaVirtualDirectory -Server $Exchange2016Server).internalurl
+$owaexturl = (Get-OwaVirtualDirectory -Server $Exchange2016Server).externalurl
+$ecpinturl = (Get-EcpVirtualDirectory -server $Exchange2016Server).internalurl
+$ecpexturl = (Get-EcpVirtualDirectory -server $Exchange2016Server).externalurl
+$ewsinturl = (Get-WebServicesVirtualDirectory -Server $Exchange2016Server).internalurl
+$ewsexturl = (Get-WebServicesVirtualDirectory -Server $Exchange2016Server).externalurl
+$easinturl = (Get-ActiveSyncVirtualDirectory -Server $Exchange2016Server).internalurl
+$easexturl = (Get-ActiveSyncVirtualDirectory -Server $Exchange2016Server).externalurl
+$oabinturl = (Get-OabVirtualDirectory -server $Exchange2016Server).internalurl
+$oabexturl = (Get-OabVirtualDirectory -server $Exchange2016Server).externalurl
+$mapiinturl = (Get-MapiVirtualDirectory -server $Exchange2016Server).internalurl
+$mapiexturl = (Get-MapiVirtualDirectory -server $Exchange2016Server).externalurl
+$OutlAnyInt = (Get-OutlookAnywhere -Server $Exchange2016Server).internalhostname
+$OutlAnyExt = (Get-OutlookAnywhere -Server $Exchange2016Server).externalhostname
+#Configure Exchange 2019 Server
+Get-OwaVirtualDirectory -Server $Exchange2019Server | Set-OwaVirtualDirectory -internalurl $owainturl -externalurl $owaexturl -Confirm:$false
+Get-EcpVirtualDirectory -server $Exchange2019Server | Set-EcpVirtualDirectory -internalurl $ecpinturl -externalurl $ecpexturl -Confirm:$false
+Get-WebServicesVirtualDirectory -server $Exchange2019Server | Set-WebServicesVirtualDirectory -internalurl $ewsinturl -externalurl $ewsexturl -Confirm:$false
+Get-ActiveSyncVirtualDirectory -Server $Exchange2019Server | Set-ActiveSyncVirtualDirectory -internalurl $easinturl -externalurl $easexturl -Confirm:$false
+Get-OabVirtualDirectory -Server $Exchange2019Server | Set-OabVirtualDirectory -internalurl $oabinturl -externalurl $oabexturl -Confirm:$false
+Get-MapiVirtualDirectory -Server $Exchange2019Server | Set-MapiVirtualDirectory -externalurl $mapiexturl -internalurl $mapiinturl -Confirm:$false
+Get-OutlookAnywhere -Server $Exchange2019Server | Set-OutlookAnywhere -externalhostname $OutlAnyExt -internalhostname $OutlAnyInt -ExternalClientsRequireSsl:$true -InternalClientsRequireSsl:$true -ExternalClientAuthenticationMethod 'Negotiate' -Confirm:$false
+Get-ClientAccessService $Exchange2019Server | Set-ClientAccessService -AutoDiscoverServiceInternalUri $autodiscoverhostname -Confirm:$false
+#Display setttings
+Get-OwaVirtualDirectory | fl server,externalurl,internalurl
+Get-EcpVirtualDirectory | fl server,externalurl,internalurl
+Get-WebServicesVirtualDirectory | fl server,externalurl,internalurl
+Get-ActiveSyncVirtualDirectory | fl server,externalurl,internalurl
+Get-OabVirtualDirectory | fl server,externalurl,internalurl
+Get-MapiVirtualDirectory | fl server,externalurl,internalurl
+Get-OutlookAnywhere | fl servername,ExternalHostname,InternalHostname
+Get-ClientAccessService | fl name,AutoDiscoverServiceInternalUri
