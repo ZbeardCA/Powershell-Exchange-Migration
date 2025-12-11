d562a3823fe28cd11f9961841b6e003f6da294ba
ZbeardCA <zaur.bahsaliev@cloud-advice.com>
2025-12-11 15:48:02 +0100

    Update Migration_Powershell.ps1

--- Migration_Powershell.ps1
+++ Migration_Powershell.ps1
@@ -1,3 +1,21 @@
+# Installation Server
+
+Download neuste SE Version
+
+##Vorbereitung:
+
+Install-WindowsFeature Server-Media-Foundation, NET-Framework-45-Features, RPC-over-HTTP-proxy, RSAT-Clustering, RSAT-Clustering-CmdInterface, RSAT-Clustering-Mgmt, RSAT-Clustering-PowerShell, WAS-Process-Model, Web-Asp-Net45, Web-Basic-Auth, Web-Client-Auth, Web-Digest-Auth, Web-Dir-Browsing, Web-Dyn-Compression, Web-Http-Errors, Web-Http-Logging, Web-Http-Redirect, Web-Http-Tracing, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Metabase, Web-Mgmt-Console, Web-Mgmt-Service, Web-Net-Ext45, Web-Request-Monitor, Web-Server, Web-Stat-Compression, Web-Static-Content, Web-Windows-Auth, Web-WMI, Windows-Identity-Foundation, RSAT-ADDS
+
+##Visual Studio 2012 und 2013
+https://www.microsoft.com/download/details.aspx?id=30679
+https://www.microsoft.com/de-de/download/details.aspx?id=40784
+
+##UCM 4.0 Runtime
+https://www.microsoft.com/de-de/download/details.aspx?id=34992
+
+## IIS URL Rewrite Module
+https://download.microsoft.com/download/1/2/8/128E2E22-C1B9-44A4-BE2A-5859ED1D4592/rewrite_amd64_en-US.msi
+
 # Umzug normales Posfach MIT Archiv
 
 New-MoveRequest `
